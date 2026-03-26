1a5a05efd68ac1843e09c06d7515b498852e2f0b
Алексей Пахомов <a.pahomov@mkskom.ru>
2026-03-26 22:43:04 +0300

    init

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <span className="text-3xl">Subscription tracker</span>
+    </div>
+  );
+}
