c75cbd95201ae6d123a30fc1529021d0be5a9a84
Алексей Пахомов <a.pahomov@mkskom.ru>
2026-03-26 22:50:56 +0300

    feat: hero ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from '@heroui/react';
+
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex flex-col flex-1 items-center justify-center font-sans bg-black gap-10">
       <span className="text-3xl">Subscription tracker</span>
+      <Button className="bg-purple-500 hover:bg-purple-400 transition-colors duration-200">
+        Click me
+      </Button>
     </div>
   );
 }
